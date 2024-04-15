55560c3443790b66d9031e48cf3cf0806a14322a
Samuel Akinboro <akinboroo@gmail..com>
2024-04-15 13:30:49 +0100

    commite

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            }),
-        };
+            });
+        }
     });
     let header = document.querySelector('header');
 
@@ -34,3 +34,28 @@ window.onscroll = () => {
     menuIcon.classList.remove('close-line');
     navbar.classList.remove('active');
 };
+
+
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+
+// ================typed js==============\\
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Graphics Designer', 'Digital Marketer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+})
